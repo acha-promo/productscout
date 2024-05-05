90e3221e8b456214ddd1e7ac99bee8dca663f866
William Comnisky <w.comnisky@gmail.com>
2024-05-05 21:26:03 +0200

    global operation timeout

--- engine.go
+++ engine.go
@@ -1,9 +1,12 @@
 package core
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"regexp"
 	"sync"
+	"time"
 )
 
 type (
@@ -23,8 +26,10 @@ type (
 	}
 
 	Config struct {
-		Debug  bool
-		Logger Logger
+		Debug          bool
+		Logger         Logger
+		MaxConcurrency int
+		Timeout        time.Duration
 	}
 
 	Engine struct {
@@ -38,37 +43,78 @@ var (
 	ErrProductNotFound = errors.New("product not found")
 )
 
-func (e *Engine) Search(product string) ([]Product, error) {
-
+func (e *Engine) init() {
 	if e.Config.Logger == nil {
 		e.Config.Logger = &DefaultLogger{}
 	}
+
+	if e.Config.MaxConcurrency == 0 {
+		e.Config.MaxConcurrency = 10
+	}
+}
+
+func (e *Engine) Search(product string) ([]Product, error) {
+	e.init()
+
+	if len(e.Scrapers) == 0 {
+		return nil, ErrMissingScraper
+	}
+
+	var ctx context.Context
+	var cancel context.CancelFunc
+	if e.Config.Timeout > 0 {
+		ctx, cancel = context.WithTimeout(context.Background(), e.Config.Timeout)
+	} else {
+		ctx, cancel = context.WithCancel(context.Background())
+	}
+	defer cancel() // Ensure all paths cancel the context to avoid context leak
 
 	var wg sync.WaitGroup
 	results := []Product{}
 	resultsMutex := &sync.Mutex{}
 
-	if len(e.Scrapers) == 0 {
-		return results, ErrMissingScraper
-	}
+	// Create a buffered channel to limit the number of goroutines
+	semaphore := make(chan struct{}, e.Config.MaxConcurrency)
 
-	for _, scraper := range e.Scrapers {
+	for idx, scraper := range e.Scrapers {
 		e.debug("Scraping", scraper.Info().URL)
+
+		select {
+		case semaphore <- struct{}{}: // Block if there are already MaxConcurrency goroutines running
+		case <-ctx.Done(): // Check if the context deadline has been reached
+			e.Config.Logger.Error(fmt.Sprintf("timeout reached before starting all scrapers (exec: %d | non-exec: %d)", idx+1, len(e.Scrapers[idx:])))
+			return results, ctx.Err()
+		}
+
 		wg.Add(1)
 		go func(s Scraper) {
 			defer wg.Done()
-			products, err := s.Scrape(product)
-			if err == nil {
-				resultsMutex.Lock()
-				results = append(results, products...)
-				resultsMutex.Unlock()
-			} else {
-				e.Config.Logger.Error(fmt.Errorf("scraper %T failed: %w", s, err))
+			defer func() { <-semaphore }() // Release the spot in the semaphore when the goroutine completes
+
+			if ctx.Err() == nil { // Check context error to avoid executing if already timed out
+				products, err := s.Scrape(product)
+				if err == nil {
+					resultsMutex.Lock()
+					results = append(results, products...)
+					resultsMutex.Unlock()
+				} else {
+					e.Config.Logger.Error(fmt.Errorf("scraper %T failed: %w", s, err))
+				}
 			}
 		}(scraper)
 	}
 
-	wg.Wait()
+	wg.Wait() // Wait for all goroutines to finish
+
+	if isGTIN(product) {
+		filteredResults := make([]Product, 0)
+		for _, result := range results {
+			if result.GTIN == product {
+				filteredResults = append(filteredResults, result)
+			}
+		}
+		results = filteredResults
+	}
 
 	return results, nil
 }
@@ -77,4 +123,8 @@ func (e *Engine) debug(args ...any) {
 	if e.Config.Debug {
 		e.Config.Logger.Debug(args...)
 	}
+}
+
+func isGTIN(s string) bool {
+	return regexp.MustCompile(`^\d{13}$`).MatchString(s)
 }
